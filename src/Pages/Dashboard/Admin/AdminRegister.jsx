30b38f30e5974585b3ee76fc7d1a8d8659703047
SandaruwanWeerawardhana <weerawadanasw@gmail.com>
2025-11-06 23:21:33 +0530

    Refactor AdminRegister and Vehicle components to use className instead of class and improve code readability

--- src/Pages/Dashboard/Admin/AdminRegister.jsx
+++ src/Pages/Dashboard/Admin/AdminRegister.jsx
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import Swal from "sweetalert2";
 
 function AdminRegister() {
-  const initialState = useState({
+  const initialState = {
     name: "",
     username: "",
     password: "",
     role: "ADMIN",
     confirmPassword: "",
-  });
+  };
 
-  const [inputData, setinputData] = useState(initialState);
+  const [inputData, setInputData] = useState(initialState);
 
   const handleChange = (e) => {
-    setinputData({ ...inputData, [e.target.name]: e.target.value });
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
@@ -41,7 +41,7 @@ function AdminRegister() {
           title: "Admin Register Success",
           icon: "success",
         });
-        setinputData(initialState);
+        setInputData(initialState);
       } else {
         Swal.fire({
           title: "Admin Register Fail!",
@@ -60,94 +60,83 @@ function AdminRegister() {
   };
 
   return (
-    <>
-      <section class="bg-gray-50 dark:bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 text-center md:text-2xl dark:text-white">
-                Register New Admin
-              </h1>
-              <form class="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-                <div>
-                  <label
-                    for="email"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Email
-                  </label>
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 text-center md:text-2xl dark:text-white">
+              Register New Admin
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Email{' '}
                   <input
                     type="email"
                     name="username"
                     value={inputData.username}
                     onChange={handleChange}
-                    class=" text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="mt-1 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Enter Your Email"
                     required=""
                   />
-                </div>
-                <div>
-                  <label class=" mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Name
-                  </label>
+                </label>
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Name{' '}
                   <input
                     type="text"
                     name="name"
                     value={inputData.name}
                     onChange={handleChange}
                     placeholder="Name"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
-                </div>
-                <div>
-                  <label
-                    for="password"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Password
-                  </label>
+                </label>
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Password{' '}
                   <input
                     type="password"
                     name="password"
                     value={inputData.password}
                     onChange={handleChange}
                     placeholder="password"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
-                </div>
-                <div>
-                  <label
-                    for="confirm-password"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Confirm password
-                  </label>
+                </label>
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Confirm password{' '}
                   <input
                     type="password"
                     name="confirmPassword"
                     value={inputData.confirmPassword}
                     onChange={handleChange}
                     placeholder="Confirm password"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
-                </div>
-                <div class="flex items-center justify-between mt-6">
-                  <button
-                    type="submit"
-                    class="text-white bg-[#2557D6] hover:bg-[#2057D6] focus:ring-4 focus:ring-[#2557D6]/50font-medium rounded-lg text-4sm px-5 py-2.5  items-center dark:focus:ring-[#2557D6]/50 me-2 mb-2 w-full text-center dark:bg-[#2557D6] dark:hover:bg-[#2057D6] dark:focus:ring-[#2557D6]/50"
-                  >
-                    ADD
-                  </button>
-                </div>
-              </form>
-            </div>
+                </label>
+              </div>
+              <div className="flex items-center justify-between mt-6">
+                <button
+                  type="submit"
+                  className="text-white bg-[#2557D6] hover:bg-[#2057D6] focus:ring-4 focus:ring-[#2557D6]/50 font-medium rounded-lg text-sm px-5 py-2.5 items-center me-2 mb-2 w-full text-center dark:bg-[#2557D6] dark:hover:bg-[#2057D6]"
+                >
+                  ADD
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
 
--- src/Pages/Dashboard/Vehicle/Vehicle.jsx
+++ src/Pages/Dashboard/Vehicle/Vehicle.jsx
@@ -52,128 +52,126 @@ function Vehicle() {
   };
 
   return (
-    <>
-      <section class="bg-white dark:bg-gray-900 mb-5">
-        <div class="py-7 px-4 mx-auto max-w-2xl lg:py-9">
-          <h2 class="mb-4 text-3xl font-bold text-gray-900 dark:text-white text-center ">
-            Vehicle Register
-          </h2>
-          <form action="#" onSubmit={handleSubmit}>
-            <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
-              <div class="w-full">
-                <label
-                  for="brand"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Brand
-                </label>
-                <input
-                  type="text"
-                  name="brand"
-                  value={inputData.brand}
-                  onChange={handleChange}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Vehicle brand"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="category"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Fuel
-                </label>
-                <select
-                  name="fuelType"
-                  value={inputData.fuelType}
-                  onChange={handleChange}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                >
-                  <option selected="">Select Fuel Type</option>
-                  <option value="Petrol">Petrol</option>
-                  <option value="Diesel">Diesel</option>
-                </select>
-              </div>
-              <div class="w-full">
-                <label
-                  for="Year"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Vehicle Register Number
-                </label>
-                <input
-                  type="text"
-                  name="registerNumber"
-                  value={inputData.registerNumber}
-                  onChange={handleChange}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder=" Register Number"
-                />
-              </div>
-              <div class="w-full">
-                <label
-                  for="Price"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Price Per Date
-                </label>
-                <input
-                  type="text"
-                  name="pricePerDay"
-                  value={inputData.pricePerDay}
-                  onChange={handleChange}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Price Per Date"
-                />
-              </div>
-
-              <div class="sm:col-span-2">
-                <label
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  for="file_input"
-                >
-                  Image
-                </label>
-                <input
-                  type="text"
-                  name="imageURl"
-                  value={inputData.imageURl}
-                  onChange={handleChange}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Image link"
-                />
-              </div>
-
-              <div class="sm:col-span-2">
-                <label
-                  for="description"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Description
-                </label>
-                <textarea
-                  name="description"
-                  value={inputData.description}
-                  onChange={handleChange}
-                  rows="8"
-                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Description"
-                ></textarea>
-              </div>
-            </div>
-            <div class="flex items-center justify-between mt-6">
-              <button
-                type="submit"
-                class="text-white bg-[#2557D6] hover:bg-[#2057D6] focus:ring-4 focus:ring-[#2557D6]/50 font-medium rounded-lg text-4sm px-5 py-2.5 items-center dark:focus:ring-[#2557D6]/50 me-2 mb-2 w-full text-center dark:bg-[#2557D6] dark:hover:bg-[#2057D6]"
+    <section className="bg-white dark:bg-gray-900 mb-5">
+      <div className="py-7 px-4 mx-auto max-w-2xl lg:py-9">
+        <h2 className="mb-4 text-3xl font-bold text-gray-900 dark:text-white text-center ">
+          Vehicle Register
+        </h2>
+        <form action="#" onSubmit={handleSubmit}>
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <div className="w-full">
+              <label
+                htmlFor="brand"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Add Vehicle
-              </button>
+                Brand
+              </label>
+              <input
+                type="text"
+                name="brand"
+                value={inputData.brand}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Vehicle brand"
+                required=""
+              />
             </div>
-          </form>
-        </div>
-      </section>
-    </>
+            <div>
+              <label
+                htmlFor="category"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Fuel
+              </label>
+              <select
+                name="fuelType"
+                value={inputData.fuelType}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              >
+                <option>Select Fuel Type</option>
+                <option value="Petrol">Petrol</option>
+                <option value="Diesel">Diesel</option>
+              </select>
+            </div>
+            <div className="w-full">
+              <label
+                htmlFor="Year"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Vehicle Register Number
+              </label>
+              <input
+                type="text"
+                name="registerNumber"
+                value={inputData.registerNumber}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder=" Register Number"
+              />
+            </div>
+            <div className="w-full">
+              <label
+                htmlFor="Price"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Price Per Date
+              </label>
+              <input
+                type="text"
+                name="pricePerDay"
+                value={inputData.pricePerDay}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Price Per Date"
+              />
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="file_input"
+              >
+                Image
+              </label>
+              <input
+                type="text"
+                name="imageURl"
+                value={inputData.imageURl}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Image link"
+              />
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="description"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Description
+              </label>
+              <textarea
+                name="description"
+                value={inputData.description}
+                onChange={handleChange}
+                rows="8"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Description"
+              ></textarea>
+            </div>
+          </div>
+          <div className="flex items-center justify-between mt-6">
+            <button
+              type="submit"
+              className="text-white bg-[#2557D6] hover:bg-[#2057D6] focus:ring-4 focus:ring-[#2557D6]/50 font-medium rounded-lg text-4sm px-5 py-2.5 items-center dark:focus:ring-[#2557D6]/50 me-2 mb-2 w-full text-center dark:bg-[#2557D6] dark:hover:bg-[#2057D6]"
+            >
+              Add Vehicle
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }
 
