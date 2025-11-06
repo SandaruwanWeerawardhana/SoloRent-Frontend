6a68338e414530b4775a14f2752e7abdb9b99779
SandaruwanWeerawardhana <weerawadanasw@gmail.com>
2025-11-06 21:58:48 +0530

    Update JSX class attributes to className and simplify data fetching in DashboardLanding

--- src/Pages/Booking/BookingForm.jsx
+++ src/Pages/Booking/BookingForm.jsx
@@ -73,31 +73,30 @@ const [inputData, setinputData] = useState(initialState);
   };
 
   return (
-    <>
-      <div className="">
-        <section class="bg-white rounded-3 bookingsize mb">
-          <div class="py-8 px-4 bg-blue-100 rounded-3 ">
-            {vehicle ? (
-              <>
+    <div className="">
+      <section className="bg-white rounded-3 bookingsize mb">
+        <div className="py-8 px-4 bg-blue-100 rounded-3 ">
+          {vehicle ? (
+            <div>
               <div className="rounded-3"> 
                 <img
-                  class="card-image img mx-auto"
+                  className="card-image img mx-auto"
                   src={vehicle.imageURl}
                   alt={vehicle.brand}
                 />
-                </div>
+              </div>
                
-                <h2 className="text-center p-1 pt-4 text-3xl">{vehicle.brand}</h2>
-              </>
-            ) : (
-              <p className="text-center text-red-500">
-                No vehicle data received.
-              </p>
-            )}
+              <h2 className="text-center p-1 pt-4 text-3xl">{vehicle.brand}</h2>
+            </div>
+          ) : (
+            <p className="text-center text-red-500">
+              No vehicle data received.
+            </p>
+          )}
 
-            <hr className="mt-3 mb-3 " />
+          <hr className="mt-3 mb-3 " />
             <form action="#" onSubmit={handleSubmit}>
-              <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+              <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
                 <TextField
                   titleName="Your Name"
                   placeholder="Your Name"
@@ -162,10 +161,10 @@ const [inputData, setinputData] = useState(initialState);
                   onChange={handleChange}
                 />
 
-                <div class="sm:col-span-2">
+                <div className="sm:col-span-2">
                   <label
-                    for="description"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    htmlFor="description"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
                   >
                     Your Other Requirement
                   </label>
@@ -175,7 +174,7 @@ const [inputData, setinputData] = useState(initialState);
                     name="description"
                     value={inputData.description}
                     onChange={handleChange}
-                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Special Requirement"
                   ></textarea>
                 </div>
@@ -184,7 +183,7 @@ const [inputData, setinputData] = useState(initialState);
               <div className="mt-3 justify-content-around">
                 <button
                   type="button"
-                  class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                  className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                   onClick={() =>
                     setinputData({
                       customerName: "",
@@ -207,7 +206,7 @@ const [inputData, setinputData] = useState(initialState);
 
                 <button
                   type="submit"
-                  class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                 >
                   Make A Reservation
                 </button>
@@ -216,7 +215,6 @@ const [inputData, setinputData] = useState(initialState);
           </div>
         </section>
       </div>
-    </>
   );
 }
 
